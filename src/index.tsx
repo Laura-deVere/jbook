1fff001d470fde68fb402cdafbe63d07f9115d90
Laura-deVere <lauradevh@gmail.com>
2023-09-17 19:34:50 -0400

    Added iframe setup

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
 
     const [input, setInput] = useState("");
     const [code, setCode] = useState("");
@@ -14,7 +15,7 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: "/esbuild.wasm"
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm"
         });
     }
 
@@ -24,6 +25,8 @@ const App = () => {
 
     const onClick = async () => {
         if(!ref.current) return;
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({ 
             entryPoints: ["index.js"], 
@@ -36,8 +39,28 @@ const App = () => {
             }
         });
         
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*")
     }
+
+    const html = `
+        <html>
+            <head></head>
+            <body>
+                <div id="root"></div>
+                <script>
+                    window.addEventListener("message", (event) => {
+                        try {
+                            eval(event.data);
+                        } catch (err) {
+                            console.error(err);
+                            const root = document.querySelector("#root");
+                            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + "<div>"
+                        }
+                    },false);
+                </script>
+            </body>
+        </html>
+    `
 
     return (
         <div>
@@ -47,6 +70,10 @@ const App = () => {
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
+            {/* sandbox no direct access between parent and child */}
+            {/* allow-same-origin | allows direct access */}
+            {/* allow-scripts | allows script execution */}
+            <iframe ref={iframe} sandbox="allow-scripts" src="/test.html" srcDoc={html}/>
         </div>
     )
 }
