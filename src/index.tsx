abb27acb5e62854703122d5575b853f4298303ee
Laura deVere-Hopkins <lauradevh@gmail.com>
2023-10-14 11:13:17 -0400

    added immer to redux reducer

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,13 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./state";
 
-import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
+import CellList from "./components/cell-list";
 
 const App = () => {
 	return (
 		<Provider store={store}>
 			<div>
-				<TextEditor />
-				{/* <CodeCell/> */}
-				{/* <CodeCell/> */}
+				<CellList />
 			</div>
 		</Provider>
 	);
